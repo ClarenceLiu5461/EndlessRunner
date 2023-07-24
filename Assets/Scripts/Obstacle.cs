f0ab7009fc3e91a4b4c8401358779e9faf9fede1
ClarenceLiu5461 <frank1213450@gmail.com>
2023-07-25 06:41:13 +0800

    Obstacle spawn and spawn pattern

---
+++ Assets/Scripts/Obstacle.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class Obstacle : MonoBehaviour
+{
+    public int damage = 1;
+    public float speed;
+
+    private void Update()
+    {
+        transform.Translate(Vector2.left * speed * Time.deltaTime);
+    }
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            //Player takes damage
+            other.GetComponent<Player>().health -= damage;
+            Debug.Log(other.GetComponent<Player>().health);
+            Destroy(gameObject);
+        }
+    }
+}
